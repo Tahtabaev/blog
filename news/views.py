a5669badd6f1dd3f9224f427989e6853f27ebdb4
Ildar <ildar.soft@gmail.com>
2017-06-21 23:34:24 +0700

    Исправление

--- news/views.py
+++ news/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from datetime import datetime, date, time
-from .forms import Newclients
+from .forms import NewclientsForm
 
 def news(request):
 
     name = "Ildar"
     date = datetime.today()
-    form = Newclients(request.POST or None)
+    form = NewclientsForm(request.POST or None)
     return render(request, 'news/news.html', locals())
